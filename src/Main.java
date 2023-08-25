4fe26102bada0167074bfd06e91d2df12299cf4d
Nazarko Bohdana <nazarko.bodhana@syk.fi>
2023-08-25 11:35:52 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        int result=0, x=5,y=-7, m=0;
+        if (y<0){
+            y=-y;
+            m=-1;
+        }
+        else {
+            m=1;
+        }
+        while (y>0){
+            if (y%2==0){
+                x=x+x;
+                y=y%2;
+            }
+            else {
+                result=result+x;
+                y=y-1;
+            }
+            if (m<0){
+                result=-result;
+            }
+        }
+        System.out.println(result);
+    }
+}
